5ebeb5acbdf0b046166279162615d99431834fab
nalini <naliniselvaraj1895@gmail.com>
2024-07-06 13:29:47 +0530

    my first git

---
+++ src/org/data/Detail.java
@@ -0,0 +1,25 @@
+package org.data;
+
+public class Detail {
+
+public static void main(String[] args) {
+	String name = "Nalini";
+	System.out.println("Student Name:"+name);
+	byte age = 21;
+	System.out.println("Student age:"+age);
+	short regno = 1234;
+	System.out.println("Student reg no:"+regno);
+	int accno = 12345678;
+	System.out.println("Student accno:"+accno);
+	long mobile = 994396775;
+	System.out.println("Student mobile no:"+mobile);
+	float cgpa = 7.52F;
+	System.out.println("Student cgpa:"+cgpa);
+	double avg = 1234.5678;
+	System.out.println("student avg:"+avg);
+	char gen = 'f';
+	System.out.println("Student gender:"+gen);
+	boolean result =true;
+	System.out.println("All above the info are:"+result);	
+}
+}
